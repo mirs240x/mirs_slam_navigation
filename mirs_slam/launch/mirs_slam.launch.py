bd983b31f47f5b26defd80ca3dfb0f62a4e9db33
oumuika <oumuika@gmail.com>
2024-11-08 17:07:12 +0900

    new launch

---
+++ mirs_slam/launch/mirs_slam.launch.py
@@ -0,0 +1,41 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import LaunchConfigurationEquals
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+
+def generate_launch_description():
+    # Declare arguments #
+    lidar_port = DeclareLaunchArgument(
+        'lidar_port', default_value='/dev/ttyUSB0',
+        description='Set lidar usb port.')
+
+    # Launch files and Nodes #
+    tf2_ros_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        parameters=['0', '0', '0.35', '1.57', '0', '0', 'base_link', 'laser']
+    )
+
+    sllidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('sllidar_ros2'),
+            'launch'),
+            '/sllidar_s1_launch.py']),
+        launch_arguments={'serial_port': lidar_port}.items(),
+        condition=LaunchConfigurationEquals('lidar', 'sllidar')
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(lidar_port)
+
+    ld.add_action(sllidar_launch)
+    ld.add_action(tf2_ros_node)
+
+    return ld
